7085c10a774e64923a2fba0f5c5b9e472146d228
Alwayshadzxyz <hadzzokeyy@gmail.com>
2026-01-05 16:02:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const canvas = document.getElementById("stars");
-document.getElementById("year").textContent = new Date().getFullYear();
-
 const ctx = canvas.getContext("2d");
 
 let w, h;
 let stars = [];
-const maxStars = 150;
+const STAR_COUNT = 120;
+let mouseX = 0, mouseY = 0;
 
 function resize() {
   w = canvas.width = window.innerWidth;
@@ -14,6 +13,7 @@ function resize() {
 window.addEventListener("resize", resize);
 resize();
 
+/* STAR OBJECT */
 class Star {
   constructor() {
     this.reset();
@@ -23,26 +23,34 @@ class Star {
     this.x = Math.random() * w;
     this.y = Math.random() * h;
     this.size = Math.random() * 1.5 + 0.3;
-    this.speed = Math.random() * 0.5 + 0.2;
+    this.speed = Math.random() * 0.3 + 0.1;
+    this.depth = Math.random() * 2 + 0.5; // parallax depth
   }
 
   update() {
     this.y += this.speed;
-    if (this.y > h) this.reset();
+    if (this.y > h) this.y = 0;
   }
 
   draw() {
+    const offsetX = (mouseX - w / 2) / (40 * this.depth);
+    const offsetY = (mouseY - h / 2) / (40 * this.depth);
+
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fillStyle = "white";
+    ctx.arc(this.x + offsetX, this.y + offsetY, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = "rgba(255,255,255,0.8)";
+    ctx.shadowBlur = 8;
+    ctx.shadowColor = "#93c5fd";
     ctx.fill();
   }
 }
 
-for (let i = 0; i < maxStars; i++) {
+/* INIT */
+for (let i = 0; i < STAR_COUNT; i++) {
   stars.push(new Star());
 }
 
+/* ANIMATE */
 function animate() {
   ctx.clearRect(0, 0, w, h);
   stars.forEach(star => {
@@ -51,5 +59,16 @@ function animate() {
   });
   requestAnimationFrame(animate);
 }
-
 animate();
+
+/* PARALLAX MOUSE */
+window.addEventListener("mousemove", e => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+/* PARALLAX TOUCH (HP) */
+window.addEventListener("touchmove", e => {
+  mouseX = e.touches[0].clientX;
+  mouseY = e.touches[0].clientY;
+});
