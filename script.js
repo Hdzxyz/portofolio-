f92fe042efdc35463692225cde988c6524edb1a9
Alwayshadzxyz <hadzzokeyy@gmail.com>
2026-01-05 15:54:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+let w, h;
+let stars = [];
+const maxStars = 150;
+
+function resize() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+}
+window.addEventListener("resize", resize);
+resize();
+
+class Star {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.x = Math.random() * w;
+    this.y = Math.random() * h;
+    this.size = Math.random() * 1.5 + 0.3;
+    this.speed = Math.random() * 0.5 + 0.2;
+  }
+
+  update() {
+    this.y += this.speed;
+    if (this.y > h) this.reset();
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+}
+
+for (let i = 0; i < maxStars; i++) {
+  stars.push(new Star());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, w, h);
+  stars.forEach(star => {
+    star.update();
+    star.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+animate();
+      
